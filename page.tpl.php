5087436c9043efa12b499e51ccb296592a150ab0
Jeff Robbins <jjeff@17190.no-reply.drupal.org>
2009-05-22 15:22:06 +0000

    Initial commit of the 960 Robots theme, an example theme created by Lullabot, based on the 960 grid system

---
+++ page.tpl.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+</head>
+  <body>
+
+    <!-- header starts-->
+    <div id="header-wrap">
+      <div id="header" class="container_16">
+        <div id="header-main">
+          <h1 id="logo-text"><a href="<?php print $front_page; ?>"><?php print $site_name; ?></a></h1>
+          <p id="slogan"><?php print $site_slogan; ?></p>
+        </div><!-- navigation -->
+        <div id="nav">
+          <?php print theme('links', $primary_links, array('class' => 'links primary-links')); ?>
+        </div>
+        <?php print $search_box; ?>
+      </div>
+    </div>
+    <!-- header ends here -->
+    
+    <!-- content starts -->
+    <div id="content-wrapper" class="container_16">
+
+    	<div id="breadcrumb" class="grid_16"><?php print $breadcrumb; ?></div>
+
+      <!-- main -->
+      <div id="main" class="grid_8">
+        <?php if (!empty($title)): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        <?php if (!empty($tabs)): ?><div class="tabs"><?php print $tabs; ?></div><?php endif; ?>
+        <?php if (!empty($messages)): print $messages; endif; ?>
+        <?php if (!empty($help)): print $help; endif; ?>
+        <div id="content-output"> 
+          <?php print $content; ?>
+        </div><!-- /#content-output -->
+      </div>
+      <!-- main ends here -->
+
+      <!-- sidebars starts here -->
+      <div id="sidebars" class="grid_8">
+
+        <!-- left sidebar starts here -->
+        <div class="grid_4 alpha sidebar-left">
+          <?php print $left; ?>
+        </div>
+        <!-- left sidebar ends here -->
+
+        <!-- right sidebar starts here -->
+        <div class="grid_4 omega sidebar-right">
+          <?php print $right; ?>
+        </div>
+        <!-- right sidebar ends here -->
+
+      </div>
+      <!-- sidebars end here -->
+
+    </div>
+    <!-- content ends here -->
+
+    <!-- footer starts here -->
+    <div id="footer-wrapper" class="container_16">
+
+      <!-- footer top starts here -->
+      <div id="footer-content">
+
+        <!-- footer left starts here -->
+        <div class="grid_8" id="footer-left">
+          <?php print $footer_left; ?>
+        </div>
+        <!-- footer left ends here -->
+
+        <!-- footer right starts here -->
+        <div class="grid_8" id="footer-right">
+          <?php print $footer_right; ?>
+        </div>
+        <!-- footer right ends here -->
+
+      </div>
+      <!-- footer top ends here -->
+
+      <!-- footer bottom starts here -->
+      <div id="footer-bottom">
+        <p class="bottom-left"><?php print $footer_message; ?></p>
+        <?php print theme('links', $secondary_links, array('class' => 'links secondary-links')); ?>
+      </div>
+      <!-- footer bottom ends here -->
+
+    </div>
+    <!-- footer ends here -->
+    <?php print $closure; ?>
+  </body>
+</html>
---
+++ template.php
@@ -0,0 +1,109 @@
+<?php
+
+function ninesixtyrobots_theme() {
+  return array(
+    'story_node_form' => array(
+      'arguments' => array('form' => NULL),
+    ),
+  );
+}
+
+function ninesixtyrobots_story_node_form($form) {
+  $published = drupal_render($form['options']['status']);
+  $buttons = drupal_render($form['buttons']);
+  $everything_else = drupal_render($form);
+  return $everything_else . $published . $buttons;  
+}
+
+function ninesixtyrobots_preprocess_node(&$vars) {
+  $node = $vars['node'];
+  $vars['date_day'] = format_date($node->created, 'custom', 'j');
+  $vars['date_month'] = format_date($node->created, 'custom', 'M');
+  $vars['date_year'] = format_date($node->created, 'custom', 'Y');
+}
+
+function ninesixtyrobots_preprocess_page(&$vars) {
+  $use_twitter = theme_get_setting('use_twitter');
+  if (is_null($use_twitter)) {
+    $use_twitter = 1;
+  }
+  
+  $query = theme_get_setting('twitter_search_term');
+  if (is_null($query)) {
+    $query = 'lullabot';
+  }
+  $query = urlencode($query);
+    
+  if ($use_twitter) {
+    $response = drupal_http_request('http://search.twitter.com/search.json?q=' . $query);
+    $data = json_decode($response->data);
+    $tweet = $data->results[array_rand($data->results)];
+    $vars['site_slogan'] = check_plain($tweet->text);
+  }
+}
+
+function ninesixtyrobots_preprocess(&$vars, $hook) {
+  $vars['classes'] = $hook;
+}
+
+function ninesixtyrobots_breadcrumb($breadcrumb) {
+  if (!empty($breadcrumb)) {
+    $breadcrumb[] = drupal_get_title();
+    $delimiter = theme_get_setting('breadcrumb_delimiter');
+    if (is_null($delimiter)) {
+      $delimiter = ' » ';
+    }
+    return '<div class="breadcrumb">'. implode($delimiter, $breadcrumb) .'</div>';
+  }
+}
+
+function ninesixtyrobots_username($object) {
+  if ($object->uid && $object->name) {
+    
+    profile_load_profile($object);
+    if (!empty($object->profile_real_name)) {
+      $object->name = $object->profile_real_name;
+    }
+    
+    // Shorten the name when it is too long or it will break many tables.
+    if (drupal_strlen($object->name) > 20) {
+      $name = drupal_substr($object->name, 0, 15) .'...';
+    }
+    else {
+      $name = $object->name;
+    }
+
+    if (user_access('access user profiles')) {
+      $output = l($name, 'user/'. $object->uid, array('attributes' => array('title' => t('View user profile.'))));
+    }
+    else {
+      $output = check_plain($name);
+    }
+  }
+  else if ($object->name) {
+    // Sometimes modules display content composed by people who are
+    // not registered members of the site (e.g. mailing list or news
+    // aggregator modules). This clause enables modules to display
+    // the true author of the content.
+    if (!empty($object->homepage)) {
+      $output = l($object->name, $object->homepage, array('attributes' => array('rel' => 'nofollow')));
+    }
+    else {
+      $output = check_plain($object->name);
+    }
+
+    $output .= ' ('. t('not verified') .')';
+  }
+  else {
+    $output = variable_get('anonymous', t('Anonymous'));
+  }
+
+  return $output;
+}
+
+function ninesixtyrobots_search_theme_form($form) {
+  $form['submit']['#type'] = 'image_button';
+  $form['submit']['#src'] = drupal_get_path('theme', 'ninesixtyrobots') . '/images/search.png';
+  $form['submit']['#attributes']['class'] = 'btn';
+  return '<div id="search" class="container-inline">' . drupal_render($form) . '</div>';
+}
